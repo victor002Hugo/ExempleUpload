18996b4739cabfebc06572847c4d2ec73632d62a
Victor Hugo <victorsouvilela@hotmail.com>
2020-06-25 18:38:29 -0300

    Fim do backend

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+
+/**
+ * Database setup
+ */
+
+mongoose.connect('mongodb://localhost:27017/upload', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(
+    "/files",
+    express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
+
+app.use(require("./routes"));
+
+app.listen(3000);
